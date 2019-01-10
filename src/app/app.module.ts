43f6d0755932fa698ea444a24954471782e5762a
Shashwat Vats <shashwatvats10@gmail.com>
2019-01-10 14:30:33 +0530

    added post-create and post-display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
-
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule,MatCardModule,MatButtonModule,MatToolbarModule,MatExpansionModule} from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { PostDisplayComponent } from './posts/post-display/post-display.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-display/post-display.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-display',
+  templateUrl: './post-display.component.html',
+  styleUrls: ['./post-display.component.css']
+})
+export class PostDisplayComponent implements OnInit {
+
+  posts = [
+    
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
